d805f770bef3411c57cc03ec0c3c17c3b6d4ac33
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-08-24 16:31:27 -0700

    Note to self

--- src/main/java/com/meepoffaith/easyflaying/util/EasyFlayingUtil.kt
+++ src/main/java/com/meepoffaith/easyflaying/util/EasyFlayingUtil.kt
@@ -6,5 +6,7 @@ object EasyFlayingUtil{
     // TODO: Maybe try to actually make a brainswept villager instead of Thanos snapping them from existence
     fun brainsweep(trader: TraderTileentity){
         trader.removeVillager()
+
+        // TODO: Do I need to add networking stuff? I'm not experienced to know when I do and don't need it. I see it in base hex brainsweep.
     }
 }
