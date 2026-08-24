cc5693cd067ca6cafde56aabda872065c966ec3e
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-08-24 16:35:27 -0700

    More notes to self

--- src/main/java/com/meepoffaith/easyflaying/util/EasyFlayingUtil.kt
+++ src/main/java/com/meepoffaith/easyflaying/util/EasyFlayingUtil.kt
@@ -1,12 +1,20 @@
 package com.meepoffaith.easyflaying.util
 
+import at.petrak.hexcasting.api.HexAPI
 import de.maxhenkel.easyvillagers.blocks.tileentity.TraderTileentity
 
 object EasyFlayingUtil{
-    // TODO: Maybe try to actually make a brainswept villager instead of Thanos snapping them from existence
+    /*
+        TODO: Maybe try to actually make a brainswept villager instead of Thanos snapping them from existence.
+              Already tried just calling HexAPI.instance().brainsweep, however it does not apply the tag.
+     */
     fun brainsweep(trader: TraderTileentity){
         trader.removeVillager()
 
-        // TODO: Do I need to add networking stuff? I'm not experienced to know when I do and don't need it. I see it in base hex brainsweep.
+        /*
+            TODO: Do I need to add networking stuff?
+                  I'm not experienced to know when I do and don't need it.
+                  I see it in base hex brainsweep code.
+         */
     }
 }
