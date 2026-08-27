185938476ce2a443b8d666e5301631060105841e
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-08-27 13:11:08 -0700

    Don't need any of that for now

--- src/main/java/com/meepoffaith/easyflaying/Config.java
+++ src/main/java/com/meepoffaith/easyflaying/Config.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
+    /*
     public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
             .comment("Whether to log the dirt block on common setup")
             .define("logDirtBlock", true);
@@ -27,6 +28,7 @@ public class Config {
     public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
+     */
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
